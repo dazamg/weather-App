6d25ced8f8d0b03147922ab850bc70f15d8da56b
dazmag <iscope09@hotmail.com>
2021-12-04 02:09:37 -0500

    setting up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+import Header from './Components/Header'
+
+import {    FormControl,
+  Button,
+  Box,
+  Flex,
+  Input,
+  InputGroup,
+} from "@chakra-ui/react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box >
+      
+      <Box 
+        w='100%'
+        minH='100vh'
+        bgImage="url('https://images.unsplash.com/photo-1534068731687-d70176c2e7d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')"
+        bgSize='contain'
+        bgRepeat='no-repeat'
+        bgPosition='-30%'
+        display='flex'
+        flexDirection='column'
+        // alignItems='center'
+        // justifyContent='center'
+        backgroundPosition="center"
+        backgroundSize="cover"
+        width="100vw"
+      >
+        <Header/>
+        <Flex align="center" justify="center" as='h2' fontSize='2xl' w='100%' fontFamily='Source Serif Pro, serif' color='rgb(237, 149, 109)' fontWeight='bold'>
+          React Weather App
+        </Flex>
+        <main className="main_content">
+          {/* add weather fetching component */}
+          <dv></dv>
+        </main>
+        <footer className="main_content">
+          Page created by yournamehere
+        </footer>
+    </Box>
+    </Box>
+    
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {    FormControl,
+    Button,
+    Box,
+    Flex,
+    Input,
+    InputGroup,
+  } from "@chakra-ui/react"
+
+
+const Header = () => {
+    return (
+        <Box as='nav' display='flex'  >
+      
+
+        <FormControl isRequired display='flex' ml='13vw'>
+            {/* <FormLabel>EMAIL</FormLabel> */}
+          <InputGroup> 
+            <Input 
+              type="search"
+              className="form-control"
+              placeholder="EMAIL"
+              focusBorderColor="rgb(237, 149, 109)"
+              variant='filled'
+            />
+          </InputGroup>
+          <Button 
+            ml='0.5vw'
+            background="rgb(75, 97, 113)" 
+            color='white'
+            className="btn"  
+    
+          >
+            Search
+          </Button>
+        </FormControl>
+        <FormControl isRequired ml='13vw'>
+            {/* <FormLabel>EMAIL</FormLabel> */}
+          <InputGroup> 
+            <Input 
+              type="search"
+              className="form-control"
+              placeholder="EMAIL"
+              focusBorderColor="rgb(237, 149, 109)"
+              variant='filled'
+            />
+          </InputGroup>
+        </FormControl>
+      </Box>
+    )
+}
+
+export default Header
